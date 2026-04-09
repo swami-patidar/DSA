ee86c1603976e0f4fce442f56eb47f021ba53100
swami <Patidar78200@gmail.com>
2026-04-09 16:46:55 +0530

    c file

---
+++ DSA (PRACTICAL/realloc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int *ptr;
+    int n, i;
+
+    // clrscr();
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    ptr = (int *)realloc(n, sizeof(int));
+
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed!");
+        getch();
+        return;
+    }
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    printf("Elements are:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+
+    free(ptr);
+
+    getch();
+}
